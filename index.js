44d074117f70d5c8a19c2fcfe11633a96b38cbbe
wshloic <lbertran@student.42lyon.fr>
2021-10-20 13:56:19 +0200

    add word ladder algorithm

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const auth = require('./auth.json');
 
 runHenry();
 
+var ladder = [];
+
+for (var n = 0; n < 10; n++) {
+	var obj = {"count": 0, "word": ""}
+	ladder[n] = obj;
+}
+
 //xaQJbozY_Is en
 //pfaSUYaSgRo en auto
 //WlVeRoSjBO8 fr auto
@@ -31,8 +38,6 @@ async function runHenry() {
 
 			var baseUrl;
 
-			console.log(captionTracks);
-
 			if (captionTracks.length == 0)
 				console.log('No captions found');
 			else {
@@ -42,14 +47,49 @@ async function runHenry() {
 						baseUrl = captionTracks[j].baseUrl;
 						break;
 					}
-				console.log(baseUrl);
 				if (baseUrl == undefined)
 					console.log('No captions available in target language');
-				else
-					console.log(await fetchCaptionsText(baseUrl));
+				else {
+					var text = await fetchCaptionsText(baseUrl);
+					for (var k = 0; k < text.length; k++)
+						updateLadder(text[k]);
+				}
+			}
+		}
+		ladder.sort((a, b) => b.count - a.count);
+		console.log(ladder);
+	}
+}
+
+function updateLadder(phrase) {
+	var words = phrase.split(' ');
+	while (words.length > 0) {
+		var word = words[0];
+		if (word.length > 4) {
+			var occurences = countOccurences(phrase, word);
+			if (ladder.filter(a => a.word == word).length > 0) {
+				var wordIndex = ladder.map(function(e) { return e.word; }).indexOf(word);
+				ladder[wordIndex].count += occurences;
+			}
+			else {
+				for (var i = 0; i < ladder.length; i++)
+					if (occurences > ladder[i].count) {
+						ladder.splice(i, 0, { "count": occurences, "word": word });
+						break;
+					}
 				}
 		}
+		words = words.filter(content => content != word);
 	}
+}
+
+function countOccurences(input, word) {
+	var occurences = 0;
+	var words = input.split(' ');
+	for (var i = 0; i < words.length; i++)
+		if (words[i] == word)
+			occurences++;
+	return occurences;
 }
 
 async function fetchCaptionsText(trackUrl) {
@@ -103,7 +143,7 @@ async function fetchChannelId(query) {
 				console.log('Channel not found');
 			else {
 				channelId = json.items[0].id.channelId;
-				console.log(channelId);
+				console.log(`Channel id : ${channelId}`);
 			}
 		});
 	return channelId;
@@ -120,10 +160,8 @@ async function fetchUploadsId(channelId) {
     		.then((json) => {
 			if (json.items == undefined)
 				console.log('Channel not found');
-			else {
+			else
 				uploadsId = json.items[0].contentDetails.relatedPlaylists.uploads;
-				console.log(uploadsId);
-			}
 		});
 	return uploadsId;
 }
@@ -139,11 +177,8 @@ async function fetchVideosId(uploadsId) {
 		if (json.items == undefined)
 			console.log('No videos found..');
 		else {
-			for (let i = 0; i < json.items.length; i++) {
-				var item = json.items[i];
-				console.log(`video #${i}: ${item.snippet.resourceId.videoId}`);
-				videosId[i] = item.snippet.resourceId.videoId;
-			}
+			for (let i = 0; i < json.items.length; i++)
+				videosId[i] = json.items[i].snippet.resourceId.videoId;
 		}
 	});
 	return videosId;
